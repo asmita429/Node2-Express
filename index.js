0f52123b202b168cdc6cb9149be3c7d26bd94767
Asmita Thapa <asmitathapa429@gmail.com>
2024-04-23 11:25:55 +0545

    handling request and response objects

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const app = express();
 
-console.dir(app);
+// console.dir(app);
 
 let port = 3000; //8080
 
 app.listen(port, () => {
   console.log(`app is listening on port ${port}`);
+});
+
+app.use((req, res) => {
+  console.log("Request received");
+  // res.send("Basic string response");
+  // res.send("<h1>html response</h1>")
+  res.send({
+    name: "apple",
+    color: "red",
+  });
 });
